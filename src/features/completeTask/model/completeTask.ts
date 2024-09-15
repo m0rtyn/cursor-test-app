37e8a202a11170ed0d3e2eacf5b5b932b5c03582
Martyn <zogacc@gmail.com>
2024-09-15 15:20:49 +0200

    fix: tsconfig

--- src/features/completeTask/model/completeTask.ts
+++ src/features/completeTask/model/completeTask.ts
@@ -1,6 +1,6 @@
 import { AppDispatch } from '../../../app/providers/store';
-import { updateTaskStatus } from '../../../entities/task/taskSlice';
-import { stopTimer } from '../../../entities/timer/timerSlice';
+import { updateTaskStatus } from '@entities/task/taskSlice';
+import { stopTimer } from '@entities/timer/timerSlice';
 
 export const completeTask = (id: string, actualDuration: number) => (dispatch: AppDispatch) => {
   dispatch(updateTaskStatus({ id, status: 'completed', actualDuration }));
